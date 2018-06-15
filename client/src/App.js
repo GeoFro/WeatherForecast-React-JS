2f730d549dc6317805b7d330919d022605ee86a5
GeoFro <georgeharoldfrost@gmail.com>
2018-06-15 21:47:42 +0100

    File structure cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
-import './App.css';
-import City from './City';
+import Form from './components/Form';
+import Title from './components/Title';
+import Weather from './components/Weather';
 
 class App extends Component {
   state = {
@@ -26,12 +26,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
+      <div>
+        <p>{this.state.response}</p>
+        <Title />
+        <Form />
+        <Weather />
       </div>
     );
   }

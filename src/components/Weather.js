6c85d709ab6f7667dc360c13e99d6ffc79229ae1
GeoFro <georgeharoldfrost@gmail.com>
2018-06-18 13:31:14 +0100

    Simplified weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Weather extends Component {
+// This is a stateless functional component.
+// It does not need to have state so it can be in the simplified form below.
 
-  render() {
-    return (
-    <div>
-      { this.props.city_name && this.props.city_country &&
-          <div>Location: {this.props.city_name}, {this.props.city_country}</div>
-      }
-      { this.props.city_temperature &&
-        <div>Current Temperature: {this.props.city_temperature} °C</div>
-      }
-      { this.props.city_description &&
-        <div>Weather Forecast: {this.props.city_description}</div>
-      }
-      { this.props.error &&
-        <div>{this.props.error}</div>
-      }
-    </div>
-    );
-  }
-}
+const Weather = props => (
+  <div>
+    { props.city_name && props.city_country &&
+        <div>Location: {props.city_name}, {props.city_country}</div>
+    }
+    { props.city_temperature &&
+      <div>Current Temperature: {props.city_temperature} °C</div>
+    }
+    { props.city_description &&
+      <div>Weather Forecast: {props.city_description}</div>
+    }
+    { props.error &&
+      <div>{props.error}</div>
+    }
+  </div>
+);
 
 export default Weather;

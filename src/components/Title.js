74ae139bce1f29400ac336e4f38652b00f1918a4
GeoFro <georgeharoldfrost@gmail.com>
2018-06-18 13:41:40 +0100

    Simple title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 
-class Title extends Component {
-
-  render() {
-    return (
-      <div>
-        This is the title component
-      </div>
-    );
-  }
-}
+const Title = () => (
+  <div>
+    This is the title component
+  </div>
+)
 
 export default Title;
